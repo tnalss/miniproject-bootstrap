46aed75805341f2eff96bb1db0a7cf7b61a98475
sumin <indoubt@naver.com>
2022-10-18 20:06:02 +0900

    toast

--- script/main.js
+++ script/main.js
@@ -1,3 +1,26 @@
 $(function () {
+    $("#searchBtn").click(function(){
+        alert("아직 이용하실 수 없는 기능입니다.");
+    })
+    $("#seeMore1").click(function(){
+        alert("DB수업 이후 가능할 것 같습니다.");
+    })
+    $("#seeMore2").click(function(){
+        alert("DB수업 이후 가능할 것 같습니다.");
+    })
 
+    // $(".buy-now").click(function(){
+
+    // })
+
+    //$('.toast').toast(option);
+
+    $('.liveToastBtn').click(function(){
+        $("#liveToast").toast('show');
+
+    })
 })
+
+
+
+
