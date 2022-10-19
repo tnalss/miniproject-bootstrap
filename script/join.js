44d884fcde2ce43a70f792dfc15bf2455bb65fb0
tnalss <indoubt@naver.com>
2022-10-19 16:40:56 +0900

    agree checkbox check

---
+++ script/join.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    let check1,check2=0;
+
+    $("#nextBtn").click(function(){    
+        if($("#agree-policy").is(":checked") == false){
+            alert("모든 약관에 동의 하셔야 다음 단계로 진행 가능합니다.");
+            return;
+        }else if($("#agree-ipolicy").is(":checked") == false){
+            alert("모든 약관에 동의 하셔야 다음 단계로 진행 가능합니다..");
+            return;
+        }else{
+            alert("가입폼은 다음에..");
+        }
+    }); 
+
+    $("#agree-policy").click(function(){
+        check1= !check1;
+        if(check1 ==1 && check2==1){
+            $("#nextBtn").removeClass("btn-secondary");
+            $("#nextBtn").addClass("btn-primary");
+        } else{
+            $("#nextBtn").removeClass("btn-primary");
+            $("#nextBtn").addClass("btn-secondary");
+        }
+    })
+    $("#agree-ipolicy").click(function(){
+        check2= !check2;
+        if(check1 ==1 && check2==1){
+            $("#nextBtn").removeClass("btn-secondary");
+            $("#nextBtn").addClass("btn-primary");
+        }else{
+            $("#nextBtn").removeClass("btn-primary");
+            $("#nextBtn").addClass("btn-secondary");
+        }
+    })
+
+    // function move_page(){
+    //     location.href="join2.html"
+    //     }
+
+
+
+
+});
