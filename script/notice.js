ff6a173d0d1c1a2b68150c1ada0d85b7a82784e6
sumin <indoubt@naver.com>
2022-10-21 00:24:29 +0900

    many upgrade

---
+++ script/notice.js
@@ -0,0 +1,25 @@
+$(function(){
+    $(".5thnoticeTitle").click(function(e){
+        e.preventDefault();
+        $(".5thnotice").toggleClass("d-none");
+    })
+    $(".4thnoticeTitle").click(function(e){
+        e.preventDefault();
+        $(".4thnotice").toggleClass("d-none");
+    })
+    $(".3thnoticeTitle").click(function(e){
+        e.preventDefault();
+        $(".3thnotice").toggleClass("d-none");
+    })
+
+    $(".2thnoticeTitle").click(function(e){
+        e.preventDefault();
+        $(".2thnotice").toggleClass("d-none");
+    })
+    $(".1thnoticeTitle").click(function(e){
+        e.preventDefault();
+        $(".1thnotice").toggleClass("d-none");
+    })
+
+
+})
